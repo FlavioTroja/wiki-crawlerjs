78a8f263c99109a3672777fe409046f5e2c2e861
Flavio Troia <flavio.troia@gmail.com>
2023-02-02 09:54:50 +0100

    (fix): Fixed import images in S3

--- s3.js
+++ s3.js
@@ -1,8 +1,7 @@
 // noinspection NpmUsedModulesInstalled
-import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
-import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { MongoClient } from "mongodb";
+import fetch from "node-fetch";
 import dotenv from "dotenv";
 
 (async () => {
@@ -16,39 +15,52 @@ import dotenv from "dotenv";
     // AWS S3 CLIENT
     const S3_REGION = process.env.S3_REGION;
     const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
-    const COGNITO_COGNITO_IDENTITY_POOL_ID = process.env.COGNITO_IDENTITY_POOL_ID;
-    const s3Client = new S3Client({
-        region: S3_REGION,
-        credentials: fromCognitoIdentityPool({
-            client: new CognitoIdentityClient({ region: S3_REGION }),
-            identityPoolId: COGNITO_COGNITO_IDENTITY_POOL_ID, // IDENTITY_POOL_ID
-        })
-    });
+    const AWS_KEY_ID = process.env.AWS_KEY_ID;
+    const AWS_KEY_SECRET = process.env.AWS_KEY_SECRET;
 
     try {
+
         await mongoClient.connect();
         console.log("Connected to database @ " + DB_URI);
 
+        const s3Client = new S3Client({
+            region: S3_REGION,
+            credentials:{
+                accessKeyId: AWS_KEY_ID,
+                secretAccessKey: AWS_KEY_SECRET
+            }
+        });
+        console.log("Connected to S3 @ " + S3_REGION);
+
         const posts = mongoClient.db(DB_NAME).collection("wp_posts");
 
-        for await(const post of posts.find({id: 41127})) {
+        for await(const post of posts.find({ "featured_media.source_url": { $not: { $regex: /https:\/\/affissioni.s3.eu-central-1.amazonaws.com.*/} } })) {
 
             if (!!post.featured_media?.source_url) {
-                process.stdout.write(`Uploading image '${post.featured_media?.source_url}' in S3`);
+                process.stdout.write(`Processing image '${post.featured_media?.source_url}' in S3`);
 
-                const fileStream = await fetch(post.featured_media?.source_url).then(response => response.blob());
-                const uploadParams = {
-                    Bucket: S3_BUCKET_NAME,
-                    // Add the required 'Key' parameter using the 'path' module.
-                    Key: "./",
-                    // Add the required 'Body' parameter
-                    Body: fileStream,
-                };
+                try {
+                    const fileStream = await fetch(post.featured_media?.source_url);
+                    const fileName = Date.now() + "-" + post.featured_media?.source_url.substring(post.featured_media?.source_url.lastIndexOf('/') + 1);
 
-                const data = await s3Client.send(new PutObjectCommand(uploadParams));
-                console.log("Success", data);
+                    await s3Client.send(new PutObjectCommand({
+                        Bucket: S3_BUCKET_NAME,
+                        // Add the required 'Key' parameter using the 'path' module.
+                        Key: fileName,
+                        // Add the required 'Body' parameter
+                        Body: await fileStream.arrayBuffer(),
+                        ContentType: fileStream.headers.get("content-type"),
+                    }));
 
-                process.stdout.write(" Done!\n");
+                    await posts.updateOne({_id: post._id}, {
+                        $set: {
+                            "featured_media.source_url": `https://${S3_BUCKET_NAME}.s3.${S3_REGION}.amazonaws.com/${fileName}`
+                        }
+                    });
+                    process.stdout.write(" Done!\n");
+                } catch (e) {
+                    process.stderr.write(" Error!\n");
+                }
             }
         }
     } finally {
